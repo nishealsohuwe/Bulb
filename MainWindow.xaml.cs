cd55dc2711419364b6c64d273a9bc5ae45de22b3
nishealsohuwe <ay.ekimovskix@gmail.com>
2023-11-23 17:52:37 +0300

    Initial commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System.Windows;
+using System.Windows.Media.Imaging;
+using System;
+
+namespace Bulb
+{
+    public partial class MainWindow : Window
+    {
+        bool bulbWorking = false;
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+        public void BulbSwitch(object sender, RoutedEventArgs e)
+        {
+            bulbWorking = !bulbWorking;
+            if (bulbWorking)
+            {
+                bulbImage.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/icons/vibrantBulb.png"));
+            }
+            else
+            {
+                bulbImage.Source = BitmapFrame.Create(new Uri(@"pack://application:,,,/icons/dimBulb.png"));
+            }
+        }
+    }
+}
